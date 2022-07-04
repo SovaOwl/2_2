8866ad71f126fa05ef546f18d52bf5e2609e69a6
SovaOwl <106985564+SovaOwl@users.noreply.github.com>
2022-07-04 20:24:37 +0300

    Update 1_2.cpp

--- 1_2/1_2.cpp
+++ 1_2/1_2.cpp
@@ -3,36 +3,38 @@
 
 using namespace std;
 
-struct address
+struct account
 {
-	string city;
-	string street;
-	int house_number = -999;
-	int apartment_number = -999;
-	int index = -999;
+	int account_number = -999;
+	string account_holder_name;
+	int balance = -999;
 };
 
-void add(address& p, string new_city, string new_street, int new_house_number, int new_apartment_number, int new_index)
+void new_balance(account& p, int new_bal)
 {
-	p.city = new_city;
-	p.street = new_street;
-	p.house_number = new_house_number;
-	p.apartment_number = new_apartment_number;
-	p.index = new_index;
-
-	cout << "Город: " << p.city << endl << "Улица: " << p.street << endl << "Номер дома: " << p.house_number << endl << "Номер квартиры: " << p.apartment_number << endl << "Индекс: " << p.index << endl << endl;
+	p.balance = new_bal;
 }
 
 int main()
 {
 	setlocale(LC_ALL, "Rus");
-	address adr;
 
-	string new_city = "Москва";
-	string new_street = "Арбат";
-	int new_house_number = 12;
-	int new_apartment_number = 8;
-	int new_index = 123456;
+	account ac;
+	int new_bal;
 
-	add(adr, new_city, new_street, new_house_number, new_apartment_number, new_index);
+	cout << "Введите номер счёта: ";
+	cin >> ac.account_number;
+
+	cout << "Введите имя владельца: ";
+	cin >> ac.account_holder_name;
+
+	cout << "Введите баланс: ";
+	cin >> ac.balance;
+
+	cout << "Введите новый баланс: ";
+	cin >> new_bal;
+
+	new_balance(ac, new_bal);
+
+	cout << "Ваш счёт: " << ac.account_number << ", " << ac.account_holder_name << ", " << ac.balance;
 }
